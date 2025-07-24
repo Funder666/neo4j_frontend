457c91faf8e4fba61cc187799c5b7ef3acd60e3b
User Restart666 <51275903005@stu.ecnu.edu.cn>
2025-07-24 13:49:34 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -17,13 +17,13 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 8888,
+    port: 8887,
     strictPort: true,
     allowedHosts: true
   },
   preview: {
     host: '0.0.0.0',
-    port: 8888,
+    port: 8887,
     strictPort: true
   }
 })
