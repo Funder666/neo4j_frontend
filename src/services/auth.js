6a7ad7bd33e46c6d558ff8875982eb25d4e54e8f
User Restart666 <51275903005@stu.ecnu.edu.cn>
2025-07-26 16:39:55 +0800

    update

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,8 +2,8 @@ class AuthService {
   constructor() {
     this.TOKEN_KEY = 'neo4j_token'
     this.USER_KEY = 'neo4j_user'
-    // this.API_BASE_URL = 'https://chineseedu.shuishan.net.cn:8000'
-    this.API_BASE_URL = 'http://localhost:8000'
+    this.API_BASE_URL = 'https://chineseedu.shuishan.net.cn:8000'
+    // this.API_BASE_URL = 'http://localhost:8000'
 
   }
 
