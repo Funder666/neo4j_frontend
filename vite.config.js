512005299ddc8ba2a22f9569f47e18f19b2e73f9
User Restart666 <51275903005@stu.ecnu.edu.cn>
2025-08-21 11:22:34 +0800

    fix(所有页面)：去掉系统下方的 vue 面板

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools' // 已移除Vue DevTools
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(), // 已禁用Vue DevTools面板
   ],
   resolve: {
     alias: {
